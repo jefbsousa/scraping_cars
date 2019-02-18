53c9249c1f32d9c9ce53df9814efb1414cae3414
jefbsousa <jef.b.sousa@gmail.com>
2019-02-17 21:08:22 -0300

    test using puppeteer to handle javascript

--- test_pup.js
+++ test_pup.js
@@ -4,14 +4,14 @@ var fs = require('fs');
 
 
 ( async () => {
-	const browser = await puppeteer.launch({hadless: false});
+	var browser = await puppeteer.launch({headless: false});
 	var page = await browser.newPage();
-	await page.setViewPort({ width: 950, height: 1450});
+	await page.setViewport({ width: 950, height: 1450});
 
-	await page.goto('https://www.carsforsale.com/Search?Radius=100&PageNumber=2&OrderBy=Relevance&OrderDirection=Desc');
-	await page.waitFor(3000);
-	console.log('Navigated to...')
-
+	await page.goto('https://www.cars.com/vehicledetail/detail/758401939/overview/'); 
+	await page.waitFor(4000); 
+	html = await page.content();
+	fs.writeFileSync('/home/jeferson/github_projects/scraping_cars/test.html', html)
 	// await browser.close()
 
 })();
