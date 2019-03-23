4b6c56c924f96de88de6ce8d1f9763ec23c59642
jefbsousa <jef.b.sousa@gmail.com>
2019-03-23 19:39:24 -0300

    scraping using grequests

---
+++ scrap.py
@@ -0,0 +1,52 @@
+import grequests
+import pandas as pd 
+import os, re
+from time import sleep, time
+
+
+df = pd.read_csv('links_py.txt', header=None)
+lst = df[0].tolist()
+lst = lst[:50]
+
+class Test():
+    def __init__(self, sublst, size):
+        self.urls = sublst
+        self.size = size
+
+    def exception(self, request, exception):
+        print ("Problem: {}: {}".format(request.url, exception))
+
+    def async_req(self):
+        return grequests.map((grequests.get(u) for u in self.urls), 
+                             exception_handler=self.exception, size= self.size)
+
+
+
+step = 10
+all_r, failed = [], []
+
+def write_html(html, n, url):
+    st = url.find('/detail/')
+    end = url.find('/overview/')
+    _id = url[st+len('/detail/'):end]
+    
+    with open(f'/home/jeferson/personal_projects/scraping_cars/results/ny/{n}-{_id}', 'wb') as f:
+        f.write(html)
+    
+n = 0
+for i in range(step-1, len(lst)+step , step):
+    st = time()
+    test_inst = Test(lst[i-step+1:i+1], step)
+    
+    res = test_inst.async_req()
+    print(res)
+    for _res in res:
+        if _res.status_code != 200:
+            failed.append(_res.url)
+        else:
+            write_html(_res.content, n, _res.url)
+    n += 1
+    print(time()-st,'\n')
+    
+#     print(lst[i-step+1:i+1])
+
--- scraper.js
+++ scraper.js
@@ -15,6 +15,11 @@ async function fetch_ads(page){
 	return await page.$$eval(selector, am => am.filter(e => e.href).map(e => e.href))
 }
 
+function write_links(file, arr){
+	file.on('error', function(err) { console.log(err) });
+	arr.forEach(function(v) { file.write(v + '\n'); });
+}
+
 
 ( async () => {
 	var browser = await puppeteer.launch({headless: false});
@@ -26,18 +31,27 @@ async function fetch_ads(page){
 	ny_url = 'https://www.cars.com/for-sale/searchresults.action/?page=1&perPage=100&rd=75&searchSource=GN_REFINEMENT&shippable-dealers-checkbox=true&showMore=false&sort=relevance&zc=10019&localVehicles=false';    
 
 	await page.goto(ny_url);
+	await page.waitFor(4000);
 
-	for(search_ind=1; search_ind <= 4; search_ind++){
+	var file = fs.createWriteStream('array2.txt');
+
+	for(search_ind=2; search_ind <= 50; search_ind++){
+		console.log('fetching links...')
 		links = await fetch_ads(page);
-		console.log(links.slice(1,3));
+		// console.log(links.slice(1,3));
+		console.log('Write to text');
+		write_links(file, links);
+
 		await page.goto(`https://www.cars.com/for-sale/searchresults.action/?page=${search_ind}&perPage=100&rd=75&searchSource=GN_REFINEMENT&shippable-dealers-checkbox=true&showMore=false&sort=relevance&zc=10019&localVehicles=false`);
-		await page.waitFor(3000);
+		await page.waitFor(4000);
 	}
 
-	await page.goto('https://www.cars.com/vehicledetail/detail/758401939/overview/'); 
-	await page.waitFor(4000); 
-	html = await page.content();
-	fs.writeFileSync('/home/jeferson/github_projects/scraping_cars/test.html', html)
-	// await browser.close()
+	file.end();
+
+	// await page.goto('https://www.cars.com/vehicledetail/detail/764209894/overview/'); 
+	// await page.waitFor(4000); 
+	// html = await page.content();
+	// fs.writeFileSync('/home/jeferson/personal_projects/scraping_cars/test.html', html)
+	await browser.close()
 
 })();
